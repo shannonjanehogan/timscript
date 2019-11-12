4f3844de251017051a4974bf4268c82734473245
Mitchell Burton <mitch@mitchellburton.ca>
2019-11-12 13:00:03 -0800

    clarify license

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject timscript "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A Music DSL"
+  :url "http://github.com/shannonjanehogan/timscript"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot timscript.core
   :target-path "target/%s"
