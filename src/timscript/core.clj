c930905d1408b64bbacfe3a59e10cb7b52e8050b
Mitchell Burton <mitch@mitchellburton.ca>
2019-11-12 16:21:21 -0800

    parsing for reverse

--- src/timscript/core.clj
+++ src/timscript/core.clj
@@ -2,7 +2,7 @@
   (:require timscript.asr
             [clojure.core.match :refer [match]])
   (:import [timscript.asr
-            Id IdNum Note Rest Chord Sequence Name Transpose LoopMe AndSeq
+            Id Num IdNum Note Rest Chord Sequence Name Transpose Loop AndSeq
             Split KeyChange Replace Reverse KeySignature ReplaceValues
             ReplaceValue])
   (:gen-class))
@@ -11,19 +11,21 @@
   "Produces an Musical Expression (ME) from a list of arguments"
   [args]
   (match [args]
-    [([Note (note-id :guard symbol?)
+    [(['Note (note-id :guard symbol?)
            (vol :guard number?)
            (dur :guard number?)] :seq)]
      (Note. note-id vol dur)
-    [([Rest (dur :guard number?)] :seq)] (Rest. dur)
-    [([Note (note-id :guard symbol?)
+    [(['Rest (dur :guard number?)] :seq)] (Rest. dur)
+    [(['Note (note-id :guard symbol?)
            (vol :guard number?)
            (dur :guard number?) & r] :seq)]
     (Sequence. (Note. note-id vol dur) (parse r))
-    [([Rest (dur :guard number?) & r] :seq)] (Sequence. (Rest. dur) (parse r))
+    [(['Rest (dur :guard number?) & r] :seq)] (Sequence. (Rest. dur) (parse r))
+    [(['reverse & r] :seq)] (Reverse. (parse r))
+    [([(bound-id :guard symbol?)] :seq)] (Id. (bound-id))
     :else (println "Unable to parse expression" args)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (parse (read-string (str "(" (first args) ")"))))
+  (println (parse (read-string (str "(" (first args) ")")))))
